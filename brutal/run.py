43c6e77246e1298522de6fd42f0b72faa0eb1aa2
Corey Bertram <corey@qr7.com>
2013-03-07 01:46:25 -0800

    cleaning up run file

--- brutal/run.py
+++ brutal/run.py
@@ -3,13 +3,17 @@ import sys
 from twisted.internet import reactor
 from twisted.python import log
 
+from brutal.core.bot import BotManager
+
 
 def main(config):
-    from brutal.core.bot import BotManager
+    """
+    this is the primary run loop, should probably catch quits here?
+    """
+    # TODO: move logging to BotManager, make configurable
+    log.startLogging(sys.stdout, setStdout=0)
+
     bot_manager = BotManager(config)
     bot_manager.start()
 
-    log.startLogging(sys.stdout)
-
     reactor.run()
-
