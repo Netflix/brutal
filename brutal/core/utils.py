d6125159e06b48df81d90ac8240336c848e54c93
Corey Bertram <corey@qr7.com>
2013-03-08 23:23:03 -0800

    core plugin class

--- brutal/core/utils.py
+++ brutal/core/utils.py
@@ -1,12 +1,12 @@
 import functools
 
+
 #TODO: not using this anymore.
 def decorator(plz_decorate):
     """
     create a decorator out of a function
     uh.. i hope this works the way i want it to work
     """
-
     @functools.wraps(plz_decorate)
     def final(func=None, **kwargs):
 
@@ -22,3 +22,16 @@ def decorator(plz_decorate):
             return decorated(func)
 
     return final
+
+
+class PluginRoot(type):
+    """
+    metaclass that all plugin base classes will use
+    """
+    def __init__(cls, name, bases, attrs):
+        if not hasattr(cls, 'plugins'):
+            # only execs when processing mount point itself
+            cls.plugins = []
+        else:
+            # plugin implementation, register it
+            cls.plugins.append(cls)
